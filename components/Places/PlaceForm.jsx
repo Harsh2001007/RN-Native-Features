84517f9003f2c019785a2561b3b35bbdd4874fb2
Harsh <hs5686584@gmail.com>
2024-05-17 00:39:17 +0530

    added input component on add place screen

--- components/Places/PlaceForm.jsx
+++ components/Places/PlaceForm.jsx
@@ -1,12 +1,57 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native';
+import React, {useState} from 'react';
+import {Colors} from '../../constants/color';
 
 export default function PlaceForm() {
+  const [text, setText] = useState('');
+
+  function inputHandler(enteredText) {
+    setText(enteredText);
+  }
   return (
-    <View>
-      <Text>PlaceForm</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={inputHandler}
+          value={text}
+        />
+        <Button
+          title="click me"
+          onPress={() => {
+            console.log(text);
+          }}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
